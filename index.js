9cbb7d899da0dae3a24e9686c3ff5b358022e458
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-06-15 15:33:29 -0300

    01:03 - Lógica inicial

--- index.js
+++ index.js
@@ -13,7 +13,44 @@ function question(query) {
     });
 }
 
-question("pergunta aleatória: ").then(function (resposta) {
-    console.log(resposta);
+const candidatos = [];
+const votos = {};
+
+async function main() {
+    console.log("### Bem vindo ao sistema eleitoral da TreinaWeb ###");
+    console.log("\nEscolha um número das opções a seguir\n");
+
+    let continuarLoop = true;
+    do {
+        console.log("1 - Cadastrar Candidato");
+        console.log("2 - Iniciar Eleição");
+        console.log("3 - Imprimir Candidatos");
+        const opcao = await question('Escolha uma opção: ')
+
+        switch (opcao) {
+            case '1':
+                await cadastrarCandidato();
+                break;
+            case '2':
+                if (candidatos.length > 1) {
+                    await iniciarEleicao();
+                    continuarLoop = false;
+                } else {
+                    console.error("\nUma eleição precisa de no minimo de dois candidatos\n");
+                }
+                break;
+            case '3':
+                console.log('\nOs candidatos são: ');
+                candidatos.forEach(function (candidato) {
+                    console.log(`Nome: ${candidato.nome}, Número: ${candidato.numero}`);
+                });
+                console.log('\n');
+                break;
+            default:
+                console.error('\nOpção inválida. Tente novamente\n');
+                break;
+        }
+
+    } while (continuarLoop)
     readline.close();
-});
+}
